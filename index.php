68381ab5db65139db2a17178a0f95978fdd0a35f
ggarcia <ggarcia@I1101>
2018-02-08 12:04:44 +0100

    primer commit guille

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ and open the template in the editor.
         
         
         echo "Esto es una primera version By oscar";
-        
+        echo "hey guille";
         ?>
     </body>
 </html>
